37415b7bf50a2ab6f5c22802cf120079f8d980bd
Gustavo <gustavosf@gmail.com>
2011-07-20 15:18:26 -0300

    Conversao do numero de matricula para 8 digitos Algumas outras adequações foram feitas para legibilidade do código :)

--- sabi.py
+++ sabi.py
@@ -9,7 +9,7 @@ baseado em uma técnica chamada "web scraping", que consiste em puxar
 informações interessantes de páginas web.
 	
 Uso:
-	s = SABi('00123456', '112233')
+	s = SABi(123456, 112233)
 	print "Livros Emprestados: " + s.loan()
 	print "Livros Reservados: " + s.hold()
 	print "Dívida: " + s.cash()
@@ -23,6 +23,13 @@ import re
 
 class SABi:
 	def __init__(self, username, password):
+		"""Construtor
+
+		Tanto username quanto password devem ser valores numéricos,
+		O sistema do SABi é baseado no sistema de matrículas da UFRGS, e 
+		portanto o username é o número da matrícula (8 dígitos, zero-fill)
+		e a senha é outro número de 6 dígitos.
+		"""
 		self.res = "http://sabi.ufrgs.br/F"
 		self.data = {
 			'loan': None,
@@ -30,15 +37,15 @@ class SABi:
 			'cash': None,
 		}
 		self.session = None
-		self.username = username
+		self.username = "%08d" % username  # zerofill
 		self.password = password
 		self.__auth()
 
 	def __auth(self):
 		"""Autenticação do Usuário
 
-		É necessário efetuar um acesso a página para puxar uma variável de sessão.
-		A variável de sessão é usada na montagem dos links, em um formato +- assim:
+		É necessário um acesso a página para puxar uma variável de sessão.
+		A esta variável é usada na montagem dos links, em um formato +- assim:
 			res + '/'' + session + '-' + randomnum(5) + '?func=' + funcao
 		Onde função é o que realmente orienta o sistema. Agumas delas abaixo:
 			- bor-info
